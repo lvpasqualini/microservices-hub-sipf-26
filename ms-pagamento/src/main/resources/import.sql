ed548d3839e93a53e2c062a754cef902c1ebc976
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-29 11:36:11 -0300

    feat: adding integration tests

---
+++ ms-pagamento/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_pagamento(valor, nome, numero_cartao, validade, codigo_seguranca, status, pedido_id) VALUES(790.0, 'Jon Snow', '6859745869874569', '12/30', '589', 'CRIADO', 1);
+INSERT INTO tb_pagamento(valor, nome, numero_cartao, validade, codigo_seguranca, status, pedido_id) VALUES(3599.0, 'Ayra Stark', '2456983214569874', '01/28', '389', 'CRIADO', 2);
