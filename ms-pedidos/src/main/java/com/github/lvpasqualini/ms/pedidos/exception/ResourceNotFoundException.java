a2aa0883afaf27687c7d15aa5205b32fc24f3f9f
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-13 09:25:43 -0300

    feat: config eureka client in ms-pagamentos and ms-pedidos

--- ms-pedidos/src/main/java/com/github/lvpasqualini/ms/pedidos/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.github.lvpasqualini.ms.pedidos.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
